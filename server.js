2f48c23000dc794903930ba1a5cba70c2eace72d
MAKSIWAY <MISCOME@YA.RU>
2016-09-26 23:12:33 +0300

    module http-server is ready

---
+++ server.js
@@ -0,0 +1,66 @@
+/*
+var cool = require('cool-ascii-faces');
+var pg = require('pg');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err) {
+	console.error(err);
+	respose.send("Error " + err);
+      }
+      else {
+	response.render('pages/db', {results: result.rows} );
+      }
+    });
+  });
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/times', function(request, response) {
+  var result = '';
+  var times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++)
+    result += i + ' ';
+  response.send(result);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+*/
+var http = require("http");
+
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+  
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
+
+
