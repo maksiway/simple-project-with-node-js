4f6386cc310793c726c48ea37e548391361652e5
MAKSIWAY <MISCOME@YA.RU>
2016-09-27 00:32:05 +0300

    module simple route is ready

--- server.js
+++ server.js
@@ -48,10 +48,15 @@ app.listen(app.get('port'), function() {
 });
 */
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    
+    route(handle, pathname);
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
