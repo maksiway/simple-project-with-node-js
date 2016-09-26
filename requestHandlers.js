f3ce6fa40418a8e0ca14daae23ea273abea13c61
MAKSIWAY <MISCOME@YA.RU>
2016-09-27 01:27:13 +0300

    route modify V1(within callback - Bad!!!)

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,23 @@
+var exec = require("child_process").exec; //не блокирующий процесс
+
 function start() {
   console.log("Request handler 'start' was called.");
-  return "Hello Start";
+
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+  
+ // sleep(10000); //вызов блокирующей функции(Node-js - однопоточная среда)
+ // return "Hello Start";
+ /*****вызов неблокирующего процесса, но он не работает, т.к. использует  callback function вместо return****/
+  var content = "empty";
+  
+  exec("ls-lah", function (error, stdout, stderr) { //асинхронная функция с stdout - function callback
+    content = stdout;
+  });
+  
+  return content; //синхронный возврат результата
 }
 
 function upload() {
