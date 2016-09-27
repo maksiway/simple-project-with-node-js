bc31f45fad2abb98e6a9bad0f86559e3ef5f7a28
MAKSIWAY <MISCOME@YA.RU>
2016-09-27 13:51:59 +0300

    router for browser answer with unblocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,28 +1,35 @@
 var exec = require("child_process").exec; //не блокирующий процесс
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-
+/*
   function sleep(milliSeconds) {
     var startTime = new Date().getTime();
     while (new Date().getTime() < startTime + milliSeconds);
   }
   
- // sleep(10000); //вызов блокирующей функции(Node-js - однопоточная среда)
- // return "Hello Start";
+  sleep(10000); //вызов блокирующей функции(Node-js - однопоточная среда)
+  return "Hello Start";
+*/
+
  /*****вызов неблокирующего процесса, но он не работает, т.к. использует  callback function вместо return****/
-  var content = "empty";
+ // var content = "empty";
   
-  exec("ls-lah", function (error, stdout, stderr) { //асинхронная функция с stdout - function callback
-    content = stdout;
+  exec("find /", { timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) { //асинхронная функция с stdout - function callback
+    //content = stdout;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
   });
-  
-  return content; //синхронный возврат результата
+  //return content; //синхронный возврат результата
 }
 
-function upload() {
+function upload(response) {
     console.log("Request handler 'upload' was called.");
-     return "Hello Upload";
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
+    //return "Hello Upload";
 }
 
 exports.start = start;
